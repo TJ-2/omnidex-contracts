ec866a74cebc8432c0e3fb122db7db69c5963f10
tj <timjudge16@gmail.com>
2022-08-02 16:42:09 +0200

    work on deposit function

--- XTLOS/depositTelos.sol
+++ XTLOS/depositTelos.sol
@@ -2,85 +2,43 @@
 
 pragma solidity 0.8.6;
 
-import "./OmniDexLoop.sol";
+import "./interfaces/IOmniDexLoop.sol";
 import "./interfaces/ICharmDojo.sol";
 import "./interfaces/ITelosEscrow.sol";
 import "./interfaces/IWTLOS.sol";
 import "./interfaces/IXTLOS.sol";
 
-
-contract checkTiers {
-
-    ITelosEscrow TelosEscrow;
-    ICharmDojo karmaContract;
-    // ILoopingContract loopingContract;
-
-    constructor(address _TelosEscrow, address _loopingContract, address _CharmDojo) public {
-        TelosEscrow = ITelosEscrow(_TelosEscrow);
-        karmaContract = ICharmDojo(_CharmDojo);
-        // loopingContract = ILoopingContract(_loopingContract);
-   }
-        
-    function getUserStakedCharmBalance(address _account) public view returns (uint256 charmAmount_) {
-        return karmaContract.charmBalance(_account); 
-    }
-
-    function getTier(address _address) public view returns(string memory result){
-        uint userBal = 100000;
-        if(userBal<100000){
-            result = "Bronze";
-        }
-        else if( userBal >= 100000 && userBal < 250000 ){
-            result = "Silver";
-        }
-        else if(userBal >= 250000 && userBal < 500000 ){
-            result = "Gold";
-        }
-        else{
-            result = "Platinum";
-        }
-        return result;
-    }
-
-    function deposit(address _address) public view returns(string memory result){
-        uint userBal = 100;
-        if(userBal<100000){
-            result = "Bronze";
-        }
-        else if( userBal >= 100000 && userBal < 250000 ){
-            result = "Silver";
-        }
-        else if(userBal >= 250000 && userBal < 500000 ){
-            result = "Gold";
-        }
-        else{
-            result = "Platinum";
-        }
-        return result;
-    }
-}
-
 contract depositTLOS {
 
-    mapping(address => uint)public balances;
+    mapping(address => uint) public balances;
+    mapping(address => uint) public XTLOSBalances;
 
     IWTLOS wrapTLOS;
     IXTLOS xTLOS;
+    IOmniDexLoop omnidexLoop;
+    ICharmDojo karmaContract;
 
-    constructor(address _wrapTLOS, address _xTLOS) public {
+    constructor(address _wrapTLOS, address _xTLOS, address _loopingContract, address _CharmDojo) public {
         wrapTLOS = IWTLOS(_wrapTLOS);
         xTLOS = IXTLOS(_xTLOS);
+        omnidexLoop = IOmniDexLoop(_loopingContract);
+        karmaContract = ICharmDojo(_CharmDojo);
+
         }
 
-    function Deposit()public payable{
+    function Deposit() public payable{
         balances[msg.sender] += msg.value;
         wrapTLOS.deposit{value: msg.value }();
+        xTLOS.mint(address(this), msg.value);
+        XTLOSBalances[msg.sender] += msg.value;
+        uint256 principal = msg.value;
+        uint256 iterations = 4;
+        omnidexLoop.enterPosition(principal, iterations);
     }
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-
 
     function withdrawMoney(uint _amount) public {
         address payable to = payable(msg.sender);
@@ -95,8 +53,26 @@ contract depositTLOS {
         // (bool sent,) = msg.sender.call{value: _amount}(“sent”);
         // require(sent, “Failed to Complete”);
     }
-}
 
+    function getTier(address _address) public view returns(string memory result){
+        // uint256 userBal = karmaContract.charmBalance(_address);
+        uint256 userBal = 100000;
+        if(userBal<100000){
+            result = "Bronze";
+        }
+        else if( userBal >= 100000 && userBal < 250000 ){
+            result = "Silver";
+        }
+        else if(userBal >= 250000 && userBal < 500000 ){
+            result = "Gold";
+        }
+        else{
+            result = "Platinum";
+        }
+        return result;
+    }
+
+}
 
     /**
      * Get function to let the user know which tier they belong to
@@ -127,10 +103,6 @@ contract depositTLOS {
        - Match solidity compiler versions to looping contract
      */
     
-
-
-
-
 
 
 
